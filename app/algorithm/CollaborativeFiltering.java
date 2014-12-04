1fe47c741f95448c62e4a1d27585a6d3984980a8
Ilia Skirko <iliaskirko@gmail.com>
2014-12-04 20:32:27 +0200

    Algorithm 1.1. Added progress, checking user's level

--- app/algorithm/CollaborativeFiltering.java
+++ app/algorithm/CollaborativeFiltering.java
@@ -1,9 +1,7 @@
 package algorithm;
 
 import controllers.Factory;
-import models.core.AnswerRecord;
-import models.core.Question;
-import models.core.User;
+import models.core.*;
 import play.Logger;
 import org.hibernate.Criteria;
 import org.hibernate.criterion.Projections;
@@ -16,7 +14,14 @@ import java.util.*;
  * Created by ilia on 31.10.14.
  */
 public class CollaborativeFiltering {
+
     private static final Logger.ALogger logger = Logger.of(CollaborativeFiltering.class);
+
+    private static Long level;
+    private static Rule rule;
+    private static double progress;
+    private static double maxProgress = 0.8;
+    private static boolean allAnswers = false;
 
     public static Question getNextQuestion(User user){
         List<AnswerRecord> answerRecords;
@@ -26,11 +31,13 @@ public class CollaborativeFiltering {
             logger.error("Error occurred when getting all answerRecords: " + ex.getMessage());
             return null;
         }
+
+        getCurrentProgress(user,answerRecords);
         //userId->(questionId->rating))
         Map<Long,Map<Long,Double>> userChoices = getChoicesRating(answerRecords);
 
         //cos measure
-        Map<Long,Double> measure = new HashMap<>();//Collections.emptyMap();
+        Map<Long,Double> measure = new HashMap<>();
         double measureSum = 0.0;
 
         double userNorm = 0.0;
@@ -80,19 +87,45 @@ public class CollaborativeFiltering {
                 }
 
         //find max
-        Long id = result.keySet().iterator().next();
+        Long id = 0L;// = result.keySet().iterator().next();
         double max = 0.0;
-        for(Long questionId : result.keySet())
-            if((!userAlreadyAnswers(user,questionId,answerRecords)) && (result.get(questionId) > max)) {
+        Question question;
+        allAnswers = false;
+        for(Long questionId : result.keySet()){
+            try {
+                question = Factory.getInstance().getQuestionDAO().getQuestionById(questionId);
+            }catch (SQLException ex) {
+                logger.error("Error occurred when getting question: " + ex.getMessage());
+                return null;
+            }
+            if((result.get(questionId) >= max) && (question.level.equals(level)) && (!userAlreadyAnswers(user,questionId,answerRecords)))  {
                 max = result.get(questionId);
                 id = questionId;
             }
-System.out.println(max);
-        System.out.println(measureSum);
-        try{
-            return Factory.getInstance().getQuestionDAO().getQuestionById(id);
         }
-        catch (SQLException ex) {
+        if(id == 0L){
+            allAnswers = true;
+            for(Long questionId : result.keySet()){
+                try {
+                    question = Factory.getInstance().getQuestionDAO().getQuestionById(questionId);
+                }catch (SQLException ex) {
+                    logger.error("Error occurred when getting question: " + ex.getMessage());
+                    return null;
+                }
+//                System.out.println(questionId + " " + result.get(questionId)+" "+(question.level.equals(level)) +" "+ (!userAlreadyAnswers(user,questionId,answerRecords)));
+                if((result.get(questionId) >= max) && (question.level.equals(level)) && (!userAlreadyAnswers(user,questionId,answerRecords))) {
+                    max = result.get(questionId);
+                    id = questionId;
+                }
+            }
+        }
+        System.out.print("id " + id + allAnswers + " " + max);
+
+        //TODO check if id is still 0L
+//        System.out.println(measureSum);
+        try {
+            return Factory.getInstance().getQuestionDAO().getQuestionById(id);
+        }catch (SQLException ex) {
             logger.error("Error occurred when getting question: " + ex.getMessage());
             return null;
         }
@@ -115,20 +148,20 @@ System.out.println(max);
         //1 if incorrect answer
         //0 if no answer
         //0.5 if correct
-        Map<Long,Map<Long,Double>> userChoices = new HashMap<>();//Collections.emptyMap();
+        Map<Long,Map<Long,Double>> userChoices = new HashMap<>();
 
         for(Long userId : userIds){
             List<AnswerRecord> userAnswers = new ArrayList<>();
             for( AnswerRecord a : answerRecords){
                 if(a.user.id.equals(userId)) userAnswers.add(a);
             }
-            Map<Long,Double> choices = new HashMap<>();//Collections.emptyMap();
+            Map<Long,Double> choices = new HashMap<>();
 
             for(AnswerRecord a : userAnswers){
-//                if(!choices.containsKey(a.question.id)){
+                if(!choices.containsKey(a.question.id)){
                     if(a.correct) choices.put(a.question.id,0.5);
                     else choices.put(a.question.id, 1.0);
-//                }else if(a.correct) choices.put(a.question.id,choices.get(a.question.id) + 0.5)
+                }else if(a.correct) choices.put(a.question.id,choices.get(a.question.id) * 0.5);
             }
 
             for(Long i : questionIds)
@@ -142,10 +175,80 @@ System.out.println(max);
     }
 
     private static boolean userAlreadyAnswers(User user,Long questionId,List<AnswerRecord> answerRecords){
-        for(AnswerRecord i : answerRecords){
-            if((i.getUser().id.equals(user.id)) && (i.question.id.equals(questionId)))
-                return true;
+        for(int i = answerRecords.size()-1; i >= 0; i--){//AnswerRecord i : answerRecords){
+            if((answerRecords.get(i).getUser().id.equals(user.id)) && (answerRecords.get(i).question.id.equals(questionId))){
+                if(answerRecords.get(i).correct) return true;
+//                if(allAnswers) return false;  //user answered to all questions, but the progress is too low to change level
+                return !allAnswers;//true;
+            }
         }
         return false;
+    }
+
+    private static void getCurrentProgress(User user, List<AnswerRecord> answerRecords){
+        int counter = 0;
+        int counterCorrect = 0;
+        Long currentLevel = 10L;      //max level
+        int i = answerRecords.size()-1;
+        level = 1L;
+        boolean ok = false;
+        do{
+            if(answerRecords.get(i).getUser().id.equals(user.id)){
+                level = answerRecords.get(i).question.level;
+                rule = answerRecords.get(i).question.rule;
+                ok = true;
+                System.out.println("id" + answerRecords.get(i).question.id);
+            }
+            i--;
+        }while ((i >= 0) &&(!ok));
+
+        i++;
+
+        if(i > 0)
+            do{
+                if(answerRecords.get(i).getUser().id.equals(user.id)){
+                    currentLevel = answerRecords.get(i).question.level;
+                    System.out.println("id" + answerRecords.get(i).question.id + currentLevel.equals(level) + (answerRecords.get(i).correct));
+                    if(currentLevel.equals(level) && (answerRecords.get(i).correct)){
+                        counterCorrect++;
+                    }
+                }
+                i--;
+            }while ((i >= 0) && (currentLevel >= level));
+System.out.println(counterCorrect);
+        counter = getQuestionCount(level);
+
+        if(counter > 0)
+            progress = (double)counterCorrect / (double)counter;
+        else
+            progress = 0.0; //there are no questions?
+
+        if(progress >= maxProgress){
+            level++;
+            allAnswers = false;
+            progress = 0.0;
+        }
+        System.out.println(progress);
+        System.out.println(level);
+    }
+
+    private static int getQuestionCount(Long level){
+        List<Long> questionIds = HibernateUtil.getSessionFactory().openSession()
+                .createCriteria(Question.class)
+                .setProjection(Projections.property("id"))
+                .list();
+
+        int count = 0;
+        for(Long id : questionIds){
+            try {
+                if(Factory.getInstance().getQuestionDAO().getQuestionById(id).level.equals(level))
+                    count++;
+            }catch (SQLException ex) {
+                logger.error("Error occurred when getting question: " + ex.getMessage());
+                return 0;
+            }
+        }
+        System.out.println(count);
+        return count;
     }
 }
